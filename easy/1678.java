ff192b3325304b63317ef224bbf7f8479c1c00cd
kylediano20 <83623904+kylediano20@users.noreply.github.com>
2022-09-25 18:55:01 -0500

    redid 1678 from memory

--- easy/1678.java
+++ easy/1678.java
@@ -1,20 +1,39 @@
 class Solution {
     public String interpret(String command) {
-		StringBuilder S = new StringBuilder(command.length());
-		for(int i = 0 ; i < command.length() ; i++){
-			if(command.charAt(i)=='G') S.append('G');
-			if(command.charAt(i)=='('){
-				if(command.charAt(i+1)==')') {S.append('o'); i++;}
-				else{S.append("al"); i = i + 3;}
+
+		StringBuilder translation = new StringBuilder(command.length()); // create a stringbuilder object with the capaity of command's length
+
+		for (int i = 0; i < command.length(); i++) {
+
+			if(command.charAt(i) == 'G'){
+
+				translation.append('G');
+
+			} else if (command.charAt(i) == '(') {
+
+				if (command.charAt(i + 1) == ')') {
+
+					translation.append('o');
+					i += 1;
+
+				} else {
+
+					translation.append("al");
+
+					i += 3;
+				}
 			}
 		}
-		return S.toString();
+
+		return translation.toString();
+
     }
 
 	public static void main(String[] args) {
 		Solution t1 = new Solution();
 
 		System.out.println(t1.interpret("G()()()()()()(al)"));
+		System.out.println(t1.interpret("G()(al)"));
 	
 	}
 }
